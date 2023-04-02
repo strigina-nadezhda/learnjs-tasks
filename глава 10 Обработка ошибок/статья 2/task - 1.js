7e632368a1231940f77b25a222ebde9072c9ae17
strigina-nadezhda <126505183+strigina-nadezhda@users.noreply.github.com>
2023-04-02 14:15:43 +0300

    Add files via upload

---
+++ глава 10 Обработка ошибок/статья 2/task - 1.js
@@ -0,0 +1,14 @@
+class FormatError extends SyntaxError {
+  constructor(message) {
+    super(message);
+    this.name = "FormatError";
+  }
+}
+
+let err = new FormatError("ошибка форматирования");
+
+console.log(err.message); // ошибка форматирования
+console.log(err.name); // FormatError
+console.log(err.stack); // stack
+
+console.log(err instanceof SyntaxError); // true
